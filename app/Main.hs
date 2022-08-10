73f3b062654193c3bdda8d86800e7c71b795f077
Chipego Kalinda <kalinda.chipego.12@gmail.com>
2022-08-10 12:21:52 +0700

    Add Data Path Parser

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,23 @@ module Main where
 
 import Options.Applicative
 
+type ItemIndex = Int
+
+itemIndexParser :: Parser ItemIndex
+itemIndexParser = argument auto (metavar "ITEMINDEX" <> help "index of item")
+
+defaultDataPath :: FilePath
+defaultDataPath = "~/.to-do.yaml"
+
+dataPathParser :: Parser FilePath
+dataPathParser = strOption $ 
+    value defaultDataPath
+    <> long "data-path"
+    <> short 'p'
+    <> metavar "DATAPATH"
+    <> help  ("path to data file (default: "++ defaultDataPath ++ ")")
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    dataPath <- execParser (info (dataPathParser) (progDesc "To-Do CLI"))
+    putStrLn $ "Data path: " ++ dataPath
